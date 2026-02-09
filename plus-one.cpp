fad029640b917536dd3754525392f68f2475c00d
Meetkhurana04 <meetwillstudy@gmail.com>
2026-02-09 23:57:56 +0530

    last minute

--- plus-one.cpp
+++ plus-one.cpp
@@ -1,23 +1,85 @@
 #include<bits/stdc++.h>
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 
-// digits 
+// digits agar 23 ho toh 24 hoajyegi simple plus one add krnah  
+// digits 1 9 h toh add krna h 1 toh woh 2 , 0 krna hoga isme 
+// last wale ko zero krenge and usse n-1 wale ko plus one 
+// toh 9 wale case me 0 kar rhe honge and but lgatar 9 aayega usko kese dekhnge
+
 int main() {
-    vector<int> digits = {1,1,2,2,9};
-    int x = digits[digits.size()-1] + 1;
-    if (x<10){
-        digits[digits.size()-1]=x;
-    }
-    else{
-        digits[digits.size()-1]=x/10; //10/10 = 0 
-        digits.push_back(x%10); // 10%10 = 1
-    }
+    vector<int> digits = {1,2,3};
+    int n = digits.size() - 1;
+    int x = digits[digits.size()-1] ; 
+   
+        // if less than 9 toh woh just plus one krdenge 
+    
+     // agar lessthan 9 nhi h toh mtlb woh 9 h toh use 0 krdenge 
+      
+        // fir se loop chlyenge n-1 ko 9 se dekhnge 
+        // if we have only one digits that is 9 toh uske liye hum new array lenge int [] new number length + 1
+        // int [] newnumber = length = (n+1) = 2 // isko bydefault zero hoga 
+        // and isme hum firs first degit 1 kar rhe honge digits[0] = 1 
 
-     for (int i = 0 ; i < digits.size() ; i ++){
-        cout << digits[i];
-    }
+        //craxy concept milgya h agr me soch rha tha ki 99 ko 100 bnana h fornt me 
+        // one kese add akr rrhogunga toh isko bdiya trika h ki hum reverse krde
+        // kyuki insertion at alst is easy than insertion at end
 
+        // toh phle digits ko reverse krdenge 
+    
+      // brute force me toh yhe hi hoga ki array se nikalo and then + 1 krke array me add krdo
+
+      cout << "brute force" << endl;
+      int sum = 0 ;
+      int i = 0 ; 
+      for(auto it : digits){
+        sum = sum * 10 + it;
+        i++;
+      }
+
+      cout << sum ;
+      sum = sum + 1;
+      // vector<int> y[];
+      // 123 
+      // 123 me se 1 ko add krna h me jaada se jaada last ko nikal skta hu 
+      // len nikalete h
+      int count =  -1; 
+      int temp = sum ;
+      while(temp>0){
+        temp=temp/10;
+        count ++ ;
+      }
+     
+      cout << endl << "question" << endl ;
+      cout << "count : " << count << endl ;
+      cout << "sum : " << sum << endl;
+      
+      // int xx = round(pow(10,count));
+      // // xx= xx/10;
+      // int yy = sum/xx;
+      // cout << "pow : " << xx << endl;
+      // cout << yy ; 
+      
+      i = 0;
+     digits.clear();   // purana digits hata do
+
+while (count >= 0) {
+    int power = round(pow(10, count));
+    int digit = sum / power;
+    digits.push_back(digit);
+    sum = sum % power;
+    count--;
+}
+      cout << "output :";
+      for(int it : digits){
+        cout << it ;
+      }
+
+
+      // while(sum>0){
+      //   y.push_back();
+      // } 
     
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,36 +1,3 @@
-#include<iostream>
-#include<vector>
-#include <map>
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-
-vector<int> n1 = {2,2,3,4,5};
-vector<int> n2 = {2,2,3,4,5};
-
-map<int ,array<int,2>> mp ;
-
-for ( int x : n1){
-    mp[x][0] = 1;
-}
-for (int x : n2){
-    mp[x][1]=1;
-}
-
-for (auto &x : mp ){
-    // map as a pair ki trh chlega
-
-    if(x.second[0]==1 && x.second[1]==1){
-        cout << x.first << " ";
-    }
-}
-
-
-
-
-
-
-
-
-}
+while(sum>0){
+      //   y.push_back();
+      // } 

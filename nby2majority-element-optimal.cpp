86da6ee3e28f1a85c1d50c0e43ad88f50786d7ba
Meetkhurana04 <meetwillstudy@gmail.com>
2026-02-18 15:54:01 +0530

    dont stop

---
+++ nby2majority-element-optimal.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> arr = {7,7,5,7,5,1,5,7,5,5,7,7,5,5,5,5};
+    int n = arr.size();
+    int el ;
+    int count = 0; 
+
+    for(int i = 0 ; i < n ; i ++){
+        if(count==0){
+            el=arr[i];
+        }
+        if(el==arr[i]){
+            count+=1;
+        }
+        else{
+            count --;
+        }
+    }
+
+    cout << el << endl;
+    count = 0 ;
+    for(int it : arr){
+         
+        if(el == it){
+            count ++;
+        }
+        
+    }
+
+    cout << el << endl;
+    cout << count << endl;
+
+    if(count > n/2){
+        cout << el << " is the majority element" << endl;
+
+    }
+    
+}

80de63ab324202bcc89e49e3511f3b79e31beeb7
Meetkhurana04 <meetwillstudy@gmail.com>
2026-02-26 23:43:56 +0530

    dont stop

--- container-with-most-water.cpp
+++ container-with-most-water.cpp
@@ -5,8 +5,16 @@ using namespace std ;
 
 int main() { 
 
-    vector<int> arr = {2,3,4,5,18,17,6};
+    vector<int> arr = {1,8,6,2,5,4,8,3,7};
+                    // 0,1,2,3,4,5,6,7,8,
     int n = arr.size() ; 
+
+    //brute toh likh diya better ya optimal dhundn h 
+    // is keliye me kya kr rha hounga ki me do aise eleemnt dhundunga jinka max sum ho 
+    // or i j me gap jada ho two sum ka recall krte h two sum me toh brute force tha 0(n2) har element ke liye 
+    // toh better merko jo yaad arha h voh kuch aisa tha ki ; two sum me toh target diya hota tah toh usse nikalte the 
+    // jiski choti height h use move kro maanlo do pointer h 1 7 
+    // chota 1 move 8 7 store kiya 7 ko move kiya 8 3 84 85 83 86             
 
     // rule h area nikalne ka ki 
     // min nikalna h  dono kisi height ka or index wale se minus krdena h 
---
+++ continer-with-most-water.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+
+using namespace std ; 
+
+int main() { 
+
+    vector<int> arr = {1,8,6,2,5,4,8,3,7};
+                    // 0,1,2,3,4,5,6,7,8,
+    int n = arr.size() ; 
+    // jo chtoa h  use move krke fix kro or sum claculate kro 
+    // jo chota h use move kro bde ko fix rkho  ; 
+    // width toh km ho hi rhi h 
+    // apka main motive h ki width jyada and hieght min me jyada gap na ho 
+    // toh here humne width km ho hi rhi h but agar merko jo chota mil rha h use m move krke bda dhunde ki koshis krunga 
+    // aise me almost saare shi combination check krlunga or max se fir unka max nikal lunga 
+    // me kuch aisa solution nhi derha hounga ki ek hi baar me max de paau / mereko super max toh nikalna hi hoga
+    int i = 0 ; 
+    int j = n-1;
+    int maxx = 0 ; 
+    int smaxx = 0 ; 
+    while(i!=j){
+        maxx = min(arr[i],arr[j]) * (j-i); 
+        smaxx = max(maxx,smaxx);
+        if(arr[i]<arr[j]){
+            i++;
+        }
+        else {
+            j--;
+        }
+
+    }
+
+    cout << smaxx ;
+
+
+
+}

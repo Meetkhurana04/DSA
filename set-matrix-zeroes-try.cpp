028f1faf4ba9138bb7df8f5739b4f4d5d39240f6
Meetkhurana04 <meetwillstudy@gmail.com>
2026-03-01 00:05:57 +0530

    dont stop

--- set-matrix-zeroes-try.cpp
+++ set-matrix-zeroes-try.cpp
@@ -29,30 +29,62 @@ for(auto row : m){
     i++;
 }
 
-
-for(auto it : ans){
+for(auto it : ans){ 
     cout << it[0] << " " << it[1] << endl;
 }
-i=0 ;
-j= 0 ;
-// ab merko jake har i = 1 or j = 1 pe 0 krna h 
-vector<vector<int>> meet ; 
+
+vector<vector<int>> meet(3, vector<int>(3));
+
+i = 0;
+j = 0 ;
 for(auto row : m){
-    int j = 0 ; 
+     j = 0;
     for(auto val : row){
-            // agar me i j store krlu toh merko bta chl jaayega 
-            meet.push_back({i,j});   
+        meet[i][j] = i*10 + j;   // ya jo pattern chaho
+        j++;
+    }
+    i++;
+}
+// i j store krliya h mene ab me chahta hu ki i j ko as a matrix ke form me print kru but ho hi nhi rha 
+// 00 01 02
+// 10 11 12
+// 20 21 22
+cout << "meet" << endl ;
+for(auto row : meet){
+    for(auto val : row){
+        cout << val << "   ";
+    }
+    cout << endl ;
+}
+
+cout << "222222" << endl ;
+
+
+// ek new array bnani h ya matrix bnani h jo origianl se leke print kre and desired jgh pe 0 kr e
+vector<vector<int>> a = m ;
+i = 0 ;
+j = 0 ;
+
+// i want to ki mer 0 11 pe h toh uske corrospondance 0 hojaye where i m workng
+for(auto &val : a){
+    j = 0;
+    for(auto &row : val){
+        if(i == 1 || j == 1){
+            row = 0;
+        }
+        
         j++;
     }
     i++;
 }
 
-cout << "meet" << endl ;
-for(auto row : meet){
-    for(auto val : row){
-        cout << val << " ";
+// & lgana jrui h inorder to deep dive or copy pe work na ho
+
+for(auto val : a){
+    for(auto row : val){
+        cout << row << " ";
     }
-    cout << endl ;
+    cout << endl;
 }
 
 }
